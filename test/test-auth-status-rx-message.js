3919230e10917175d62cd76c975d363dd53679c4
Paul Dickens <dickens.paul@gmail.com>
2017-04-11 17:17:12 +1000

    Update test-auth-status-rx-message.js

--- test/test-auth-status-rx-message.js
+++ test/test-auth-status-rx-message.js
@@ -2,10 +2,25 @@ var should = require('chai').should()
 var AuthStatusRxMessage = require('../lib/messages/auth-status-rx-message')
 
 describe('AuthStatusRxMessage', function() {
-  it('should not throw', function() {
-    (function() {
-      var data = Buffer.from("050101", "hex")
+  describe('constructor', function() {
+    it('should parse a properly constructed message', function() {
       var message = new AuthStatusRxMessage(data);
-    }).should.not.throw();
+      message.authenticated.should.equal(1);
+      message.bonded.should.equal(1);
+    });
+    
+    it('should throw if the first byte is not 0x5', function() {
+      (function() {
+        var data = Buffer.from("000000", "hex")
+        var message = new AuthStatusRxMessage(data);
+      }).should.throw();
+    });
+
+    it('should throw if the message is shorter than three bytes', function() {
+      (function() {
+        var data = Buffer.from("05", "hex")
+        var message = new AuthStatusRxMessage(data);
+      }).should.throw();
+    });
   });
 });
