be2db69682dff2480169991f472a2cdea84d1bca
Paul Dickens <dickens.paul@gmail.com>
2017-04-11 10:44:19 +1000

    Update test-auth-status-rx-message.js

--- test/test-auth-status-rx-message.js
+++ test/test-auth-status-rx-message.js
@@ -1,1 +1,11 @@
+var should = require('chai').should 
+var AuthStatusRxMessage = require('../lib/message/auth-status-rx-message')
 
+describe('AuthStatusRxMessage', function() {
+  it('should not throw', function() {
+    (function() {
+      var data = Buffer.from("050101", "hex")
+      var message = new AuthStatusRxMessage(data);
+    }).should.not.throw();
+  });
+});
