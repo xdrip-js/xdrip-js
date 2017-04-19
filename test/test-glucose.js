7f1584879fa48852e8dafadf0ea71ec349b63301
Paul Dickens <dickens.paul@gmail.com>
2017-04-19 22:21:37 +1000

    rework Glucose object, remove moment depdendency

--- test/test-glucose.js
+++ test/test-glucose.js
@@ -1,5 +1,4 @@
 var should = require('chai').should;
-var moment = require('moment');
 
 var Glucose = require('../lib/glucose');
 var TransmitterTimeRxMessage = require('../lib/messages/transmitter-time-rx-message');
@@ -9,29 +8,28 @@ var TransmitterStatus = require('../lib/transmitter-status');
 
 describe('Glucose', function() {
   var timeMessage;
-  var activationDate;
+  var syncDate;
 
   before(function() {
     var data = Buffer.from('2500470272007cff710001000000fa1d', 'hex');
     timeMessage = new TransmitterTimeRxMessage(data);
 //    activationDate = new Date(2016, 10, 1);
-    activationDate = moment.utc({y: 2016, M: 9, d: 1});
-    console.log(activationDate);
+//    syncDate = moment.utc({y: 2016, M: 9, d: 1});
+    syncDate = Date.UTC(2016, 6, 17); // 17 July 2016 (months are 0 - 11)
+
+//    console.log(activationDate);
   });
 
   it('should parse message data', function() {
     var data = Buffer.from('3100680a00008a715700cc0006ffc42a', 'hex');
     var message = new GlucoseRxMessage(data);
-    var glucose = new Glucose(message, timeMessage, activationDate);
+    var glucose = new Glucose(message, timeMessage, syncDate);
     glucose.status.should.equal(TransmitterStatus.ok);
-//    glucose.readDate.should.equal(new Date(2016, 12, 6, 7, 51, 38));
-    glucose.readDate.format().should.equal(moment.utc({y: 2016, M: 11, d: 6, h: 7, m: 51, s: 38}).format());
-//    glucose.readDate.should.equal(new moment({y: 2016, M: 9, d: 2}));
-//    glucose.sessionStartDate.should.equal(new Date(2016, 12, 26, 11, 16, 12));
-    glucose.sessionStartDate.format().should.equal(moment.utc({y: 2016, M: 11, d: 26, h: 11, m: 16, s: 12}).format());
+    glucose.state.should.equal(CalibrationState.ok);
+    // there are 1740989 seconds between the glucose timestamp and the current time in the above hex strings
+    glucose.readDate.should.equal(syncDate - 1740989 * 1000);
     glucose.isDisplayOnly.should.be.false;
     glucose.glucose.should.equal(204);
-    glucose.state.should.equal(CalibrationState.ok);
     glucose.trend.should.equal(-1);
   });
 });
