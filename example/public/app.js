4226fd7e2661a231ccb5d7d1bbe4446ee29853a8
Paul Dickens <dickens.paul@gmail.com>
2017-04-22 00:21:43 +1000

    add calibration state to angular app

--- example/public/app.js
+++ example/public/app.js
@@ -6,10 +6,12 @@ factory('transmitterSocket', function (socketFactory) {
 }).
 controller('MyCtrl', ['$scope', 'transmitterSocket', function ($scope, transmitterSocket) {
   transmitterSocket.on('glucose', function(glucose) {
+    // simpler just to do $scope.glucose = glucose?
     $scope.transmitterAge = glucose.timeMessage.currentTime;
     $scope.sensorAge = glucose.timeMessage.sessionStartTime;
     $scope.time =  new Date(glucose.readDate);
     $scope.glucose = glucose.glucose;
+    $scope.state = glucose.state;
   });
   transmitterSocket.on('id', function(value) {
     $scope.id = value;
@@ -28,5 +30,32 @@ filter('mg_per_dl', function() {
 filter('mmol_per_L', function() {
  return function(glucose) {
     return glucose ? (glucose/18).toFixed(1) + ' mmol/L' : '--';
+  };
+}).
+filter('state', function() {
+ return function(state) {
+//   let formatted;
+   switch (state) {
+     case 1:
+       return "Stopped";
+       break;
+     case 2:
+       return "Warmup";
+       break;
+     case 4:
+       return "First calibration";
+       break;
+     case 5:
+       return "Second calibration";
+       break;
+     case 6:
+       return "OK";
+       break;
+     case 7:
+       return "Need calibration";
+       break;
+     default:
+       return "--";
+     }
   };
 });
