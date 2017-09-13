1a490f9457a0ab5dc920c15d4b55668bafb11842
Paul Dickens <dickens.paul@gmail.com>
2017-09-13 12:11:59 +1000

    fix error

--- example/index.js
+++ example/index.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000;
 const server = express()
   .use(express.static('example/public'))
   .use('/node_modules', express.static('node_modules'))
+  .use('/css', express.static('node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
