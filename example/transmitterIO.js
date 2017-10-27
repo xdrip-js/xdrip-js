a5f41f164be3e31a7eda1f8191775b2518e2ef05
Paul Dickens <dickens.paul@gmail.com>
2017-10-28 09:07:50 +1100

    wrap xDripAPS

--- example/transmitterIO.js
+++ example/transmitterIO.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const xDripAPS = require("./xDripAPS")();
 
 module.exports = (io, transmitter) => {
   let lastGlucose;
@@ -6,64 +7,9 @@ module.exports = (io, transmitter) => {
   transmitter.on('glucose', glucose => {
     lastGlucose = glucose;
     console.log('got glucose: ' + glucose.glucose);
-    
-    // log error and ignore errant glucose values
-    if (glucose.glucose > 800 || glucose.glucose < 20) {
-      console.log('Invalid glucose value received from transmitter, ignoring');
-    }
-    else {
-      
+
     io.emit('glucose', glucose);
-
-    // TODO: move this xDripAPS POST code to another file or function
-    const entry = [{
-      'device': 'DexcomR4',
-      'date': glucose.readDate,
-      'dateString': new Date(glucose.readDate).toISOString(),
-      'sgv': Math.round(glucose.glucose),
-      'direction': 'None',
-      'type': 'sgv',
-      'filtered': glucose.filtered,
-      'unfiltered': glucose.unfiltered,
-      'rssi': "100", // TODO: consider reading this on connection and reporting
-      'noise': "1",
-      'trend': glucose.trend,
-      'glucose': Math.round(glucose.glucose)
-    }];
-
-    const data = JSON.stringify(entry);
-    const secret = process.env.API_SECRET;
-
-    const options = {
-      hostname: '127.0.0.1', // could also try localhost ?
-      port: 5000,
-      path: '/api/v1/entries',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Content-Length': Buffer.byteLength(data),
-        'API-SECRET': secret
-      }
-    };
-
-    const req = http.request(options);
-
-    // const req = http.request(options, function(res) {
-    //   // console.log('Status: ' + res.statusCode);
-    //   // console.log('Headers: ' + JSON.stringify(res.headers));
-    //   res.setEncoding('utf8');
-    //   // res.on('data', function (body) {
-    //   //   console.log('Body: ' + body);
-    //   // });
-    // });
-
-    req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
-    });
-
-    req.write(data);
-    req.end();
-    }
+    xDripAPS.post(glucose);
   });
 
   io.on('connection', (socket) => {
