ec78ec0a8531c23de4483a565dc902d3ce5c2fe0
Paul Dickens <dickens.paul@gmail.com>
2017-10-31 20:06:53 +1100

    add charts

--- example/public/app.js
+++ example/public/app.js
@@ -4,7 +4,8 @@ var app = angular.module('AngularOpenAPS', [
   'ngRoute',
   // 'ngTouch',
   'mobile-angular-ui',
-  'btford.socket-io'
+  'btford.socket-io',
+  'chart.js'
 ])
 
 app.config(function($routeProvider, $locationProvider) {
@@ -57,6 +58,36 @@ app.controller('MyCtrl', ['$scope', 'transmitterSocket', function ($scope, trans
   transmitterSocket.on('id', function(value) {
     $scope.id = value;
   });
+
+  // for demo chart
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40]
+    // [28, 48, 40, 19, 86, 27, 90]
+  ];
+  // $scope.onClick = function (points, evt) {
+  //   console.log(points, evt);
+  // };
+  // $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
 }])
 
 app.filter('time', function() {
