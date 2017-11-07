3543540897b6bdf4a5692dea92c8d6a043934f88
Paul Dickens <dickens.paul@gmail.com>
2017-11-07 22:27:59 +1100

    set debugging within app

--- app/index.js
+++ app/index.js
@@ -3,6 +3,10 @@ const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
 
+// bad hack - can't work out how to set this from crontab
+// TODO: fix this
+process.env.DEBUG = 'transmitter'
+
 const server = express()
   .use(express.static('app/public'))
   .use('/node_modules', express.static('node_modules'))
