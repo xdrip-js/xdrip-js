c2d17b784fb9c415395705bd65b49134dbe9dd07
fidoman <26698840+efidoman@users.noreply.github.com>
2017-11-30 14:32:24 -0500

    Updating to support one-shot mode
    Updates to support one-shot mode

--- example/index.js
+++ example/index.js
@@ -4,7 +4,38 @@ const id = process.argv[2];
 const transmitter = new Transmitter(id);
 
 transmitter.on('glucose', glucose => {
-  console.log('got glucose: ' + glucose.glucose);
+  //console.log('got glucose: ' + glucose.glucose);
+  lastGlucose = glucose;
+  var d= new Date(glucose.readDate);
+  var fs = require('fs');
+  const entry = [{
+      'device': 'DexcomR4',
+      'date': glucose.readDate,
+      'dateString': new Date(glucose.readDate).toISOString(),
+      'sgv': Math.round(glucose.unfiltered),
+      'direction': 'None',
+      'type': 'sgv',
+      'filtered': Math.round(glucose.filtered),
+      'unfiltered': Math.round(glucose.unfiltered),
+      'rssi': "100", // TODO: consider reading this on connection and reporting
+      'noise': "1",
+      'trend': glucose.trend,
+      'glucose': Math.round(glucose.glucose)
+    }];
+    const data = JSON.stringify(entry);
+
+  if(glucose.unfiltered > 500 || glucose.unfiltered < 30) // for safety, I'm assuming it is erroneous and ignoring
+    {
+      console.log("Error - bad glucose data, not processing");
+      process.exit();
+    }
+    fs.writeFile("entry.json", data, function(err) {
+    if(err) {
+        console.log("Error while writing entry-test.json");
+        console.log(err);
+        }
+    process.exit();
+    }); 
 });
 
 transmitter.on('disconnect', process.exit);
