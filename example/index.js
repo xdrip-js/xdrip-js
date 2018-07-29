e0da5d8e57205285ff254508d5e7f169981c0f3f
Paul Dickens <dickens.paul@gmail.com>
2018-07-29 14:57:40 +1000

    refactor transmitter.js to use async/await, ES6 class and eslint

--- example/index.js
+++ example/index.js
@@ -1,10 +1,10 @@
+/* eslint-disable no-console */
 const Transmitter = require('..');
 
 const id = process.argv[2];
 const transmitter = new Transmitter(id);
 
-transmitter.on('glucose', glucose => {
-  console.log('got glucose: ' + glucose.glucose);
-});
+transmitter.on('glucose', glucose => console.log(`got glucose: ${glucose.glucose}, filtered: ${glucose.filtered}, unfiltered: ${glucose.unfiltered}`));
+transmitter.on('calibrationData', calibration => console.log(`got calibrationData: ${calibration}`));
 
 transmitter.on('disconnect', process.exit);
