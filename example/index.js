39fec1b7bc31a158b5d188de58884ba6680fc18c
Paul Dickens <dickens.paul@gmail.com>
2018-07-29 19:51:16 +1000

    refactor based on eslint suggestions

--- example/index.js
+++ example/index.js
@@ -2,9 +2,14 @@
 const Transmitter = require('..');
 
 const id = process.argv[2];
-const transmitter = new Transmitter(id);
+const getMessages = () => [{
+  date: Date(),
+  type: 'BatteryStatus',
+}];
+const transmitter = new Transmitter(id, getMessages);
 
 transmitter.on('glucose', glucose => console.log(`got glucose: ${glucose.glucose}, filtered: ${glucose.filtered}, unfiltered: ${glucose.unfiltered}`));
 transmitter.on('calibrationData', calibration => console.log(`got calibrationData: ${calibration}`));
+transmitter.on('batteryStatus', status => console.log(`battery status: ${status.status}, voltagea: ${status.voltagea}, voltageb: ${status.voltageb}`));
 
 transmitter.on('disconnect', process.exit);
