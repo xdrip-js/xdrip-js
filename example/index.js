db58f7da86d39c79635c8b455942989949761dd2
Paul Dickens <dickens.paul@gmail.com>
2018-08-04 08:58:22 +1000

    create new dates properly

--- example/index.js
+++ example/index.js
@@ -8,7 +8,7 @@ const getMessages = () => [{
 }];
 const transmitter = new Transmitter(id, getMessages);
 
-transmitter.on('glucose', glucose => console.log(`got glucose: ${glucose.glucose}, filtered: ${glucose.filtered}, unfiltered: ${glucose.unfiltered}`));
+transmitter.on('glucose', glucose => console.log(`got glucose at ${glucose.readDate}: ${glucose.glucose}, filtered: ${glucose.filtered}, unfiltered: ${glucose.unfiltered}`));
 transmitter.on('calibrationData', calibration => console.log(`calibration data: last calibrated at ${calibration.glucose} on ${calibration.date}`));
 transmitter.on('batteryStatus', status => console.log(`battery status: ${status.status}, voltagea: ${status.voltagea}, voltageb: ${status.voltageb}`));
 
